9c03eee3e15ee2f00fa19e4874f65198823e7752
Sam <horppes.web@gmail.com>
2018-09-15 04:23:11 +0100

    Update Keys.cs

--- Brings Canteen/Keys/Keys.cs
+++ Brings Canteen/Keys/Keys.cs
@@ -7,13 +7,13 @@ namespace Brings_Canteen.Keys
 {
     public class Google
     {
-        public static string ClientID = "261347498097-60j38s7pm8i1t4odsh8i04ta3k0rhrhf.apps.googleusercontent.com";
-        public static string ClientSecret = "ncF1-ZI-Qs3T3r5YASe4SvKY";
+        public static string ClientID = "Replace this with your Client ID";
+        public static string ClientSecret = "and Client Secret";
     }
 
     public class Facebook
     {
-        public static string AppID = "495820270896959";
-        public static string AppSecret = "68210d1f462bf3cc695dfe576cb8e081";
+        public static string AppID = "Replace this with your Client ID";
+        public static string AppSecret = "and Client Secret";
     }
 }
